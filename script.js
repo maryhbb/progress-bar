0c27a2cd9c5e57c496c521c8b35f365ce7d0b60a
Mary <mary_hbb@yahoo.com>
2023-06-04 16:53:00 +0200

    Last Update

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-const viewportHeight = window.innerHeight;
+// const viewportHeight = window.innerHeight;
 
+// window.addEventListener("scroll", () => {
+//     const body = document.body;
 
-window.addEventListener("scroll", () => {
-    const body = document.body;
+//   const scrollHeight = body.getBoundingClientRect().top;
+//   const min = Math.min(0 , viewportHeight + scrollHeight)
+// //   console.log(scrollHeight);
+//   console.log(min)
+// //   console.log(viewportHeight)
 
-  const scrollHeight = body.getBoundingClientRect().top;
-  const min = Math.min(0 , viewportHeight + scrollHeight)
-//   console.log(scrollHeight);
-  console.log(min)
-//   console.log(viewportHeight)
+// });
 
-});
+const progressEl = document.querySelector(".progressbar");
 
+const onScroll = () => {
+  const scrolled = window.scrollY;
+  //   console.log(scrolled);
+
+  const scrollHeight = document.documentElement.scrollHeight;
+  //  console.log(scrollHeight)
+
+  const viewportHeight = window.innerHeight;
+  // console.log(viewportHeight);
+
+  const scrollable = scrollHeight - viewportHeight;
+//   console.log(scrollable);
+
+  const scrolledInPercent = Math.round((scrolled * 100) / scrollable);
+  console.log(scrolledInPercent);
+
+  progressEl.style.backgroundImage = `linear-gradient(to right, red ${scrolledInPercent}%, blue ${scrolledInPercent}%)`;
+};
+
+window.addEventListener("scroll", onScroll);
+onScroll();
